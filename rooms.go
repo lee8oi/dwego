2f308dc63db843f718793389c03a22946e14f653
Lee Forest <lee8oi@gmail.com>
2014-03-15 15:37:59 -0400

    Added third room.

--- rooms.go
+++ rooms.go
@@ -15,8 +15,15 @@ func init() {
 	rooms[1].Exit["north"] = 2
 	rooms[2] = Room{
 		ID:          2,
-		Description: "The second room! There's an exit to the south.",
+		Description: "The second room! There's an exit to the south and to the east.",
 		Exit:        make(map[string]int),
 	}
 	rooms[2].Exit["south"] = 1
+	rooms[2].Exit["east"] = 3
+	rooms[3] = Room{
+		ID:          2,
+		Description: "The third room! There's an exit to the west.",
+		Exit:        make(map[string]int),
+	}
+	rooms[3].Exit["west"] = 2
 }
